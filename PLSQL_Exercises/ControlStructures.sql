ae88ae37050d0cc7480b7c2823edf923a57ef1f2
Aryan Gupta <145272881+aryangupta1708@users.noreply.github.com>
2026-07-27 12:14:58 +0530

    update code

--- PLSQL_Exercises/ControlStructures.sql
+++ PLSQL_Exercises/ControlStructures.sql
@@ -3,25 +3,19 @@
 --The bank wants to provide a 1% discount on loan interest rates for customers who are more than 60 years old.
 --A FOR LOOP is used to go through all customers. If a customer's age is greater than 60, the loan interest rate is reduced by 1%.
 
-SET SERVEROUTPUT ON;
-
 BEGIN
     FOR c IN (
-        SELECT CustomerID
+        SELECT CustomerID, DOB
         FROM Customers
-        WHERE Age > 60
     ) LOOP
-
-        UPDATE Loans
-        SET InterestRate = InterestRate - 1
-        WHERE CustomerID = c.CustomerID;
-
+        IF FLOOR(MONTHS_BETWEEN(SYSDATE, c.DOB)/12) > 60 THEN
+            UPDATE Loans
+            SET InterestRate = InterestRate - 1
+            WHERE CustomerID = c.CustomerID;
+        END IF;
     END LOOP;
 
     COMMIT;
-
-    DBMS_OUTPUT.PUT_LINE('Loan interest updated successfully.');
-
 END;
 /
 
@@ -30,26 +24,19 @@ END;
 --Customers with a balance greater than $10,000 are promoted to VIP status.
 --The program checks each customer's account balance and updates the IsVIP field to TRUE if the balance meets the required condition.
 
-SET SERVEROUTPUT ON;
-
 BEGIN
-
     FOR c IN (
-        SELECT CustomerID
+        SELECT CustomerID, Balance
         FROM Customers
-        WHERE Balance > 10000
     ) LOOP
-
-        UPDATE Customers
-        SET IsVIP = 'TRUE'
-        WHERE CustomerID = c.CustomerID;
-
+        IF c.Balance > 10000 THEN
+            UPDATE Customers
+            SET IsVIP = 'Y'
+            WHERE CustomerID = c.CustomerID;
+        END IF;
     END LOOP;
 
     COMMIT;
-
-    DBMS_OUTPUT.PUT_LINE('VIP status updated successfully.');
-
 END;
 /
 
@@ -58,33 +45,18 @@ END;
 --The bank needs to remind customers whose loan due date is within the next 30 days.
 --The PL/SQL block retrieves all such customers and prints a reminder message using DBMS_OUTPUT.PUT_LINE.
 
-SET SERVEROUTPUT ON;
-
 BEGIN
-
     FOR l IN (
-
-        SELECT c.Name,
-               l.LoanID,
-               l.DueDate
-
+        SELECT c.Name, LoanID, EndDate
         FROM Customers c
         JOIN Loans l
-
         ON c.CustomerID = l.CustomerID
-
-        WHERE l.DueDate BETWEEN SYSDATE AND SYSDATE + 30
-
+        WHERE EndDate BETWEEN SYSDATE AND SYSDATE + 30
     ) LOOP
-
         DBMS_OUTPUT.PUT_LINE(
-            'Reminder: Dear ' || l.Name ||
-            ', your Loan ID ' || l.LoanID ||
-            ' is due on ' || l.DueDate
+            l.Name || ' - Loan ' || l.LoanID ||
+            ' due on ' || TO_CHAR(l.EndDate, 'DD-MON-YYYY')
         );
-
     END LOOP;
-
 END;
 /
-
